c3862e35318039c6e77dddc243cc551ff5668f1a
MagicLab34 <155476360+MagicLab34@users.noreply.github.com>
2024-01-03 16:48:54 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2709
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 // script.js\
+\
+// Fonction pour d\'e9marrer l'exp\'e9rience\
+function startExperience() \{\
+    // Logique pour la s\'e9quence dynamique de couleurs\
+    // ...\
+\
+    // Attendre pendant 5 secondes\
+    setTimeout(function() \{\
+        // Inviter les participants au sondage\
+        inviteToSurvey();\
+    \}, 5000);\
+\}\
+\
+// Fonction pour inviter les participants au sondage\
+function inviteToSurvey() \{\
+    // Afficher le sondage et attendre les r\'e9ponses\
+    // ...\
+\
+    // Apr\'e8s la r\'e9ception des r\'e9ponses, afficher le r\'e9sultat\
+    displayResult();\
+\}\
+\
+// Fonction pour afficher le r\'e9sultat final\
+function displayResult() \{\
+    // Afficher le r\'e9sultat final (smiley, redirection, etc.)\
+    // ...\
+\}\
+}
